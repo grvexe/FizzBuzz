5c42b962602a88325c2dd0761a60f562d523047e
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-08 14:05:25 +0530

    add code for fizzbuzz from 1 to 100 in console as a comment

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+FizzBuzz
+• print numbers from 1 to 100
+• if number is divisible by 3 then print Fizz
+• if number is divisible by 5 then print Buzz
+• if number is divisible by both 3 and 5 then print FizzBuzz
+*/
+
+/*
+Code for FizzBuzz from 1 to 100 in console
+for(let i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log(`${i} FizzBuzz`);
+    }
+    else if(i % 3 == 0){
+        console.log(`${i} Fizz`);
+    }
+    else if(i % 5 == 0){
+        console.log(`${i} Buzz`);
+    }
+    else{
+        console.log(i);
+    }
+}
+*/
