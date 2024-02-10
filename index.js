2467299e1a64f16950fcc1a3f9e72f01bbe09794
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-10 20:27:24 +0530

    create textbox, button and label on html that accepts numbers and displays fizz, buzz and fizzbuzz accordingly

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ FizzBuzz
 • if number is divisible by both 3 and 5 then print FizzBuzz
 */
 
+document.getElementById("mybtn").onclick = function fizzBuzz(){
+    let i = document.getElementById("txtbox").value;
+    
+
+    if(i % 3 == 0 && i % 5 == 0){
+        document.getElementById("labeltxt").innerHTML = "FizzBuzz";
+    }
+    else if(i % 3 == 0){
+        document.getElementById("labeltxt").innerHTML = "Fizz";
+    }
+    else if(i % 5 == 0){
+        document.getElementById("labeltxt").innerHTML = "Buzz";
+    }
+    else{
+        document.getElementById("labeltxt").innerHTML = i;
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Code for FizzBuzz from 1 to 100 in console
 for(let i = 1; i <= 100; i++){
